1888859d0164a04795c82e528e6fa1f453d5b72e
Mark Nakayama <mark.nakay@gmail.com>
2022-10-18 17:19:15 -0600

    Added inhereitance, class attributes and methods

--- starter/cupcakes.py
+++ starter/cupcakes.py
@@ -1,4 +1,9 @@
-class Cupcake:
+from abc import ABC, abstractmethod
+
+
+class Cupcake(ABC):
+    size = "regular"
+
     def __init__(self, name, price, flavor, frosting, filling):
         self.name = name
         self.price = price
@@ -11,9 +16,33 @@ class Cupcake:
         for sprinkle in args:
             self.sprinkles.append(sprinkle)
 
+    @abstractmethod
+    def calculate_price(self, quantity):
+        return quantity * self.price
+
 
 my_cupcake = Cupcake("Cookies and Cream", 2.99, "Chocolate", "Oreo", "Vanilla")
 
 my_cupcake.add_sprinkles("Oreo crumbs", "Chocolate", "Vanilla")
 
 print(my_cupcake.sprinkles)
+
+
+class Mini(Cupcake):
+    size = "mini"
+
+    def __init__(self, name, price, flavor, frosting):
+        self.name = name
+        self.price = price
+        self.flavor = flavor
+        self.frosting = frosting
+        self.sprinkles = []
+
+    def calculate_price(self, quantity):
+        return quantity * self.price
+
+
+my_cupcake_mini = Mini("Chocolate", 1.99, "Mixed Chocolate", "Ganache")
+print(my_cupcake_mini.name)
+print(my_cupcake_mini.price)
+print(my_cupcake_mini.size)
