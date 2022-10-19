4e1f9774fb78799eed4a67885e78d6e21183e695
Mark Nakayama <mark.nakay@gmail.com>
2022-10-19 17:28:38 -0600

    added functions to read, overwrite, and append sample.csv

--- starter/cupcakes.py
+++ starter/cupcakes.py
@@ -1,8 +1,13 @@
+# Imports the abstract method into the file
 from abc import ABC, abstractmethod
+
+# Imports pretty prin into the file, allowing the csv files to be formatted in the terminal
+from pprint import pprint
+
+# Importing the csv fies for the realted funcitons to run
 import csv
 
-with open("sample.csv") as csvfile:
-    pass
+# Parent Class with an Abstract requiring calculate_price function
 
 
 class Cupcake(ABC):
@@ -24,12 +29,17 @@ class Cupcake(ABC):
     def calculate_price(self, quantity):
         return quantity * self.price
 
+# Child Class for a Regular cupcake with required abstract from parent class
 
-my_cupcake = Cupcake("Cookies and Cream", 2.99, "Chocolate", "Oreo", "Vanilla")
 
-my_cupcake.add_sprinkles("Oreo crumbs", "Chocolate", "Vanilla")
+class Regular(Cupcake):
+    size = "regular"
 
-print(my_cupcake.sprinkles)
+    def calculate_price(self, quantity):
+        return quantity * self.price
+
+
+# Child Class defining initiates and fields for a mini cupcake with required abstract from parent class
 
 
 class Mini(Cupcake):
@@ -46,7 +56,87 @@ class Mini(Cupcake):
         return quantity * self.price
 
 
+# Child Class for a Large cupcake with required abstract from parent class
+class Large(Cupcake):
+    size = "large"
+
+    def calculate_price(self, quantity):
+        return quantity * self.price
+
+
+# Instance of a mini cupcake with print functions to confirm creation
 my_cupcake_mini = Mini("Chocolate", 1.99, "Mixed Chocolate", "Ganache")
 print(my_cupcake_mini.name)
 print(my_cupcake_mini.price)
 print(my_cupcake_mini.size)
+
+# Cupcake instances
+cupcake1 = Regular("Apple Pie", 3.39,
+                   "cinnamon-apple", "cinnamon buttercream", "fresh apple filling")
+cupcake1.add_sprinkles("Red", "White", "Chocolate")
+cupcake2 = Mini("Country Cherry Cheesecake", 2.39,
+                "graham cracker", "vanilla buttercream")
+cupcake2.add_sprinkles("country cherry filling")
+cupcake3 = Large("Lemon Drop", 5.39, "lemon cake",
+                 "lemon buttercream", "fresh lemon curd")
+
+cupcake_list = [
+    cupcake1,
+    cupcake2,
+    cupcake3
+]
+
+# Function to write over the sample.csv file and create a new list based on the instances above
+
+
+def write_new_csv(file, cupcakes):
+    with open(file, "w", newline="\n") as csvfile:
+        fieldnames = ["size", "name", "price", "flavor",
+                      "frosting", "sprinkles", "filling"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for cupcake in cupcakes:
+            if hasattr(cupcake, "filling"):
+                writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor,
+                                "frosting": cupcake.frosting, "filling": cupcake.filling, "sprinkles": cupcake.sprinkles})
+            else:
+                writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price,
+                                "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
+
+
+# write_new_csv("sample.csv", cupcake_list)
+# Un-note the above line to write an entirely new list to the sample.csv file
+
+
+# Function to append the sample.csv file and add new instances to the list
+def add_cupcake(file, cupcake):
+    with open(file, "a", newline="\n") as csvfile:
+        fieldnames = ["size", "name", "price", "flavor",
+                      "frosting", "sprinkles", "filling"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        if hasattr(cupcake, "filling"):
+            writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor,
+                            "frosting": cupcake.frosting, "filling": cupcake.filling, "sprinkles": cupcake.sprinkles})
+        else:
+            writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price,
+                            "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
+
+
+add_cupcake("sample.csv", cupcake_list)
+# Un-note the above line to write an entirely new list to the sample.csv file
+
+# Function to read the csv files
+
+
+def read_csv(file):
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            pprint(row)
+
+# read_csv("sample.csv")
+# Un-note the above line to read csv files to the terminal
